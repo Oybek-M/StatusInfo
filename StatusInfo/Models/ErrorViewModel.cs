89c18dc0b9479923cc72fc941b99ce59bfe50e11
Oybek <muxtaraliyevoybek@gmail.com>
2024-08-31 12:12:08 +0500

    Add project files.

---
+++ StatusInfo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StatusInfo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
